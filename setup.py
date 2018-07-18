d66ba5a8f2aa3404b106115666bd746edbfd7ff8
anki-xyz <me@anki.xyz>
2018-07-18 22:06:49 +0200

    Create package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='lif_reader',
+      version='0.1',
+      author='Andreas Kist',
+      author_email='anki@neuro.mpg.de',
+      packages=find_packages(),
+      install_requires=['javabridge', 'deepdish'])
